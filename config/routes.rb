f8879b370c183720a56ae6f1694e8fc91c27bdb3
Brice Sanchez <brice@bisscomm.com>
2015-07-28 16:06:38 -0400

    Add update_positions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Refinery::Core::Engine.routes.draw do
   namespace :admin, :path => Refinery::Core.backend_route do
-    resources :resource_categories, :except => :show
+    resources :resource_categories, :except => :show do
+      post :update_positions, on: :collection
+    end
   end
 end
